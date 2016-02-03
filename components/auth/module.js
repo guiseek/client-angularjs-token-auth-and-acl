0ccaf12619010d5e89a6bfbf035ef4f201fa6a0d
Guilherme Siquinelli <guiseek@gmail.com>
2016-02-03 02:04:55 -0200

    Seed project

--- components/auth/module.js
+++ components/auth/module.js
@@ -8,11 +8,11 @@
         })
         .run(function($rootScope,$location,Storage) {
             $rootScope.$on('$routeChangeStart',function(e, next, current) {
-                if (!Storage.get('token')) {
-                    $location.url('/signin');
-                }
                 if (Storage.getObject('user')) {
                     $rootScope.logged = Storage.getObject('user');
+                }
+                if (!Storage.get('token')) {
+                    $location.url('/signin');
                 }
             });
         });
